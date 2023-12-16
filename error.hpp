60d210206d7629ec3bec089ae1f508c873821288
efypuyot <92715199+efypuyot@users.noreply.github.com>
2023-12-16 17:05:45 +0800

    started error handling on parser

--- error.hpp
+++ error.hpp
@@ -1,9 +1,9 @@
 #ifndef ERROR_HPP
 #define ERROR_HPP
 
-#include <iostream> 
+#include <iostream>
 #include <string>
-#include <list> 
+#include <list>
 
 #include "parser.hpp"
 #include "table.hpp"
@@ -11,72 +11,82 @@
 enum class ErrorType;
 std::string ErrorTypeToString(ErrorType type);
 
-enum class ErrorType { 
-    SYNTAX_ERROR, // grammar violation
-    SEMANTIC_ERROR, // type checking, expected datatype
-    TYPE_ERROR, // incompatible data types
-    RUNTIME_ERROR, // unexpected conditions or invalid operations
+enum class ErrorType
+{
+    SYNTAX_ERROR,    // grammar violation
+    SEMANTIC_ERROR,  // type checking, expected datatype
+    TYPE_ERROR,      // incompatible data types
+    RUNTIME_ERROR,   // unexpected conditions or invalid operations
     REFERENCE_ERROR, // accessing variable before declaration
-    SCOPE_ERROR,  // unbalanced braces
-    FUNCTION_ERROR, // invalid parameters
+    SCOPE_ERROR,     // unbalanced braces
+    FUNCTION_ERROR,  // invalid parameters
 };
 
-
-class Error {
+class Error
+{
 public:
-    Error(ErrorType type, const std::string& message, int line)
-        : type_(type), message_("\t======== ERROR FOUND ========\n" + ErrorTypeToString(type) + " on or after line " + std::to_string(line) + ": " + message + "\n"), hasError_(true) {}
+    Error(ErrorType type, const std::string &message, int line)
+        : type_(type), message_("\n\t======== ERROR FOUND ========\n" + ErrorTypeToString(type) + " on or after line " + std::to_string(line) + ": " + message + "\n"), hasError_(true) {}
     Error() : type_(ErrorType::SYNTAX_ERROR), hasError_(false) {}
-    
-    std::string getMessage() const {
+
+    std::string getMessage() const
+    {
         return message_;
     }
-    bool hasError() const {
+    bool hasError() const
+    {
         return hasError_;
     }
 
-    ErrorType getType() const {
+    ErrorType getType() const
+    {
         return type_;
     }
+
 private:
     ErrorType type_;
     std::string message_;
     bool hasError_;
 };
 
-
-std::string ErrorTypeToString(ErrorType type) {
-    switch(type) {
-        case ErrorType::SYNTAX_ERROR: return "Syntax Error";
-        case ErrorType::SCOPE_ERROR: return "Scope Error";
-        case ErrorType::FUNCTION_ERROR: return "Function Error";
-        case ErrorType::RUNTIME_ERROR: return "Runtime Error";
-        case ErrorType::SEMANTIC_ERROR: return "Semantic Error";
-        default: return "Unknown Error";
+std::string ErrorTypeToString(ErrorType type)
+{
+    switch (type)
+    {
+    case ErrorType::SYNTAX_ERROR:
+        return "Syntax Error";
+    case ErrorType::SCOPE_ERROR:
+        return "Scope Error";
+    case ErrorType::FUNCTION_ERROR:
+        return "Function Error";
+    case ErrorType::RUNTIME_ERROR:
+        return "Runtime Error";
+    case ErrorType::SEMANTIC_ERROR:
+        return "Semantic Error";
+    default:
+        return "Unknown Error";
     }
 }
 
 #endif
 
-/* Other Notes: 
+/* Other Notes:
     - line counter still inconsistent
         - doesn't work for blocks/scope
 
     - not sure how to implement with asm
 */
 
-
-
-/* 
+/*
 List of errors (in order of parser.hpp):
-    1. invalid declaration 
+    1. invalid declaration
         1. expected keyword LET
         2. expected identifier
         3. expected data type
     2. token does not match expected token
         1. expected ;
         2. expected \n
-        3. expected = 
+        3. expected =
         4. invalid parameter
         5. invalid expression
         6. not enough operands for operator
@@ -101,6 +111,4 @@ List of errors (in order of parser.hpp):
         1. expected keyword RETURN
 */
 
-
 /* try to pass index and check whole code which index the error is found*/
-
