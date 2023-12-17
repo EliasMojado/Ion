03133608a0aa90fdd92ba17f1dc629ca0e9f668c
efypuyot <92715199+efypuyot@users.noreply.github.com>
2023-12-17 12:13:28 +0800

    Delete line_count.hpp

--- line_count.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef LINE_COUNT_HPP
-#define LINE_COUNT_HPP
-#include "parser.hpp"
-// std::cout << line_counter << std::endl;
-extern int *line_count;
-// &line_count = line_counter;
-
-#endif // LINE_COUNT_HPP
