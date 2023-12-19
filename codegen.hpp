286102e560bc954fe468d0d4ac59260035bff5ba
John Elias Mojado <121659441+EliasMojado@users.noreply.github.com>
2023-12-19 13:36:03 +0800

    final na jud

--- codegen.hpp
+++ codegen.hpp
@@ -114,7 +114,7 @@ void generate_code(AST_program *program, std::string programName)
     asmFile << "    intFormat db '%d', 0  ; Format string for integers\n\n";
     asmFile << "    buffer rb 64\n\n";
     asmFile << "    intstore rq 0\n";
-    asmFile << "    newline db " ", 0DH, 0AH, 0\n";
+    asmFile << "    newline db ' ', 0DH, 0AH, 0\n";
     asmFile << "    charstore db ' '\n";
     asmFile << "    boolstore rb 1\n";
     asmFile << "    stringstore db ' ', 0\n\n";
